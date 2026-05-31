185e96c1c13ec91d318b60195be64d86b18dd9d3
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-31 16:35:40 +0800

    Refactor to clean up code

--- frontend/src/components/RecordedActivityCard.jsx
+++ frontend/src/components/RecordedActivityCard.jsx
@@ -16,30 +16,26 @@ const RecordedActivityCard = (props) => {
   const { _id: id, type, activity_date, distance_m, duration_ms, laps, intensity_level, comments } = props.data;
   const { distance_m_toggle, duration_ms_toggle, laps_toggle, intensity_level_toggle } = props.config || defaultConfig;
   return (
-    <div className={`${css["rec-activity-card-container"]}`}>
-      <div className={`${css["rec-activity-card-header"]}`}>
-        <div className={`${css["header-info"]}`}>
-          <img
-            className={`${css["rec-activity-type-icon"]}`}
-            src={getActivityIcon(type)}
-            alt={`icon image for ${type}`}
-          />
+    <div className={css["rec-activity-card-container"]}>
+      <div className={css["rec-activity-card-header"]}>
+        <div className={css["header-info"]}>
+          <img className={css["rec-activity-type-icon"]} src={getActivityIcon(type)} alt={`icon image for ${type}`} />
           <div>
-            <div className={`${css["header-title"]}`}>{type}</div>
-            <div className={`${css["header-sub"]}`}>{getDateAndTime(formatForDateTimeLocal(activity_date))[0]}</div>
+            <div className={css["header-title"]}>{type}</div>
+            <div className={css["header-sub"]}>{getDateAndTime(formatForDateTimeLocal(activity_date))[0]}</div>
           </div>
         </div>
-        <button className={`${css["action-icon-button"]}`}>
+        <button className={css["action-icon-button"]}>
           <img
-            className={`${css["button-icon"]}`}
+            className={css["button-icon"]}
             src={getAsset(iconEditSrc)}
             alt="edit icon"
             onClick={() => props.onClick(id)}
           />
         </button>
       </div>
-      <div className={`${css["comment"]}`}>{comments}</div>
-      <div className={`${css["stat-list"]}`}>
+      <div className={css["comment"]}>{comments}</div>
+      <div className={css["stat-list"]}>
         {distance_m_toggle && <StatDisplay label="Distance" value={distance_m} />}
         {duration_ms_toggle && <StatDisplay label="Duration" value={getDurationString(duration_ms)} />}
         {laps_toggle && <StatDisplay label="Laps" value={laps} />}
