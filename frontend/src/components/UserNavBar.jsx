d1b3cd8f0431fb6a23cf808f23d0409c1fde16ea
leslietan1981 <leslietan.yw1981@gmail.com>
2026-05-31 15:53:59 +0800

    Refactor to use assetUtils

--- frontend/src/components/UserNavBar.jsx
+++ frontend/src/components/UserNavBar.jsx
@@ -2,25 +2,21 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router";
 import css from "../styles/HomePage.module.css";
 import UserContext from "../context/UserContext.js";
-
-import logoImage from "../assets/images/nav-logo.png";
-import profileImage from "../assets/images/default-profile-portrait.png";
+import { defaultProfileSrc, getAsset, navLogoSrc } from "../utils/assetUtils.js";
 
 const UserNavBar = () => {
   const userCtx = useContext(UserContext);
 
   return (
-    <nav className={`${css["nav-container"]}`}>
-      <img className={`${css["nav-logo"]}`} src={logoImage} />
-      <div className={`${css["nav-links"]}`}>
-        <div className={`${css["nav-primary"]}`}>
-          <NavLink to="/user/dashboard" data-text="Dashboard">
-            Dashboard
-          </NavLink>
+    <nav className={css["nav-container"]}>
+      <img className={css["nav-logo"]} src={getAsset(navLogoSrc)} />
+      <div className={css["nav-links"]}>
+        <div className={css["nav-primary"]}>
+          <NavLink to="/user/dashboard">Dashboard</NavLink>
         </div>
-        <div className={`${css["nav-secondary"]}`}>
-          <div className={`${css["profile-link"]}`}>
-            <img className={`${css["profile-img"]}`} src={profileImage} />
+        <div className={css["nav-secondary"]}>
+          <div className={css["profile-link"]}>
+            <img className={css["profile-img"]} src={getAsset(defaultProfileSrc)} />
             <NavLink to="/user/profile" data-text="Profile">
               {userCtx.displayName}
             </NavLink>
--- frontend/src/utils/assetUtils.js
+++ frontend/src/utils/assetUtils.js
@@ -2,6 +2,8 @@ export const iconEditSrc = "/images/FeEdit.svg";
 export const iconAddSrc = "/images/IcRoundAdd.svg";
 export const iconCloseSrc = "/images/IcRoundClose.svg";
 export const iconDeleteSrc = "/images/TablerTrash.svg";
+export const navLogoSrc = "/images/nav-logo.png";
+export const defaultProfileSrc = "/images/default-profile-portrait.png";
 
 export const getAsset = (src) => {
   return new URL("../assets" + src, import.meta.url).href;
